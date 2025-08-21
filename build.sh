72e7d810e87258694a003f5e50468baf1ee2d89d
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2025-08-21 12:15:16 -0400

    Use parcel for processing CSS.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-mkdir -p dist/fonts
+mkdir -p src/woff2
 
 fontforge -script convert-pfb.pe putr8a utopia-regular
 fontforge -script convert-pfb.pe putri8a utopia-italic
 fontforge -script convert-pfb.pe putb8a utopia-bold
 fontforge -script convert-pfb.pe putbi8a utopia-bold-italic
 
-npx lightningcss-cli --minify src/utopia.css --output-file dist/utopia.css
+npx parcel build src/utopia.css --dist-dir dist --no-source-maps
