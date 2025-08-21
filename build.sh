5a9e9097175fd0075a5494920557373a75baa515
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2025-08-21 11:20:01 -0400

    Add source and minified CSS files.

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ fontforge -script convert-pfb.pe putr8a utopia-regular
 fontforge -script convert-pfb.pe putri8a utopia-italic
 fontforge -script convert-pfb.pe putb8a utopia-bold
 fontforge -script convert-pfb.pe putbi8a utopia-bold-italic
+
+npx lightningcss-cli --minify src/utopia.css --output-file dist/utopia.css
