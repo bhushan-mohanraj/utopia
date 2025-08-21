70ce0f058294e32ee5b558a07e673958bf3e19ac
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2025-08-21 10:46:46 -0400

    Convert PFB files to WOFF2 files.

---
+++ build.sh
@@ -0,0 +1,4 @@
+fontforge -script convert-pfb.pe putr8a utopia-regular
+fontforge -script convert-pfb.pe putri8a utopia-italic
+fontforge -script convert-pfb.pe putb8a utopia-bold
+fontforge -script convert-pfb.pe putbi8a utopia-bold-italic
